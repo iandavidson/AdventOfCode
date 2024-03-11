c77eb3229443f7bbd79b0e44ab70cf35a9c1205b
Ian Davidson <Ianmattdavidson@gmail.com>
2024-03-11 08:39:51 -0700

    working on day 2, most likley need to change approach

--- src/main/java/org/example/advent/year2023/twentyone/StepCounter.java
+++ src/main/java/org/example/advent/year2023/twentyone/StepCounter.java
@@ -26,7 +26,7 @@ public class StepCounter {
 
         try {
             ClassLoader classLoader = StepCounter.class.getClassLoader();
-            File file = new File(Objects.requireNonNull(classLoader.getResource(INPUT_PATH)).getFile());
+            File file = new File(Objects.requireNonNull(classLoader.getResource(SAMPLE_INPUT_PATH)).getFile());
             Scanner myReader = new Scanner(file);
             int i = 0;
             while (myReader.hasNextLine()) {
@@ -154,10 +154,112 @@ public class StepCounter {
         return walk(grid, start);
     }
 
+    private static List<WalkState> getNeighborsPart2(List<List<TILE>> grid, WalkState walkState){
+        List<WalkState> walkStates = new ArrayList<>();
+
+        //use this for mod: Math.floorMod( , )
+
+        //up
+        if(grid.get((walkState.getRow() -1) % grid.size()).get(walkState.getCol() % grid.get(0).size()) != TILE.ROCK){
+//        if (isInBounds(grid, walkState.getRow() - 1, walkState.getCol())) {
+            walkStates.add(WalkState.builder()
+                    .coordinate(
+                            Coordinate.builder()
+                                    .row(walkState.getRow() - 1)
+                                    .col(walkState.getCol())
+                                    .build())
+                    .stepsRemaining(walkState.getStepsRemaining() - 1)
+                    .build());
+        }
+
+        //down
+        if(grid.get((walkState.getRow() +1) % grid.size()).get(walkState.getCol() % grid.get(0).size()) != TILE.ROCK){
+//        if (isInBounds(grid, walkState.getRow() + 1, walkState.getCol())) {
+            walkStates.add(WalkState.builder()
+                    .coordinate(
+                            Coordinate.builder()
+                                    .row(walkState.getRow() + 1)
+                                    .col(walkState.getCol())
+                                    .build())
+                    .stepsRemaining(walkState.getStepsRemaining() - 1)
+                    .build());
+        }
+
+        //right
+        if(grid.get(walkState.getRow() % grid.size()).get((walkState.getCol() + 1) % grid.get(0).size()) != TILE.ROCK){
+//        if (isInBounds(grid, walkState.getRow(), walkState.getCol() + 1)) {
+            walkStates.add(WalkState.builder()
+                    .coordinate(
+                            Coordinate.builder()
+                                    .row(walkState.getRow())
+                                    .col(walkState.getCol() + 1)
+                                    .build())
+                    .stepsRemaining(walkState.getStepsRemaining() - 1)
+                    .build());
+        }
+
+        //left
+//        if (isInBounds(grid, walkState.getRow(), walkState.getCol() - 1)) {
+        if(grid.get(walkState.getRow() % grid.size()).get((walkState.getCol() - 1) % grid.get(0).size()) != TILE.ROCK){
+            walkStates.add(WalkState.builder()
+                    .coordinate(
+                            Coordinate.builder()
+                                    .row(walkState.getRow())
+                                    .col(walkState.getCol() - 1)
+                                    .build())
+                    .stepsRemaining(walkState.getStepsRemaining() - 1)
+                    .build());
+        }
+
+        return walkStates;
+    }
+
+    private static long walkPart2(List<List<TILE>> grid, WalkState start){
+        Set<WalkState> seenStates = new HashSet<>();
+
+        Queue<WalkState> queue = new LinkedList<>();
+        queue.add(start);
+        seenStates.add(start);
+        long count = 0;
+
+        while (!queue.isEmpty()) {
+            WalkState current = queue.remove();
+
+            if (current.getStepsRemaining() == 0) {
+                count++;
+
+            } else {
+                seenStates.remove(current);
+                List<WalkState> neighbors = getNeighborsPart2(grid, current);
+                for (WalkState walkState : neighbors) {
+                    if (!seenStates.contains(walkState)) {
+                        seenStates.add(walkState);
+                        queue.add(walkState);
+                    }
+                }
+            }
+
+        }
+
+        return count;
+    }
+
+    public long part2(){
+        List<List<TILE>> grid = readFile();
+        WalkState start = WalkState.builder()
+                .coordinate(
+                        Coordinate.builder().row(startRow).col(startCol).build())
+                .stepsRemaining(500).build();
+
+        return walkPart2(grid, start);
+    }
+
 
     public static void main(String[] args) {
         StepCounter stepCounter = new StepCounter();
-        log.info("Part1: " + stepCounter.part1());
+//        log.info("Part1: " + stepCounter.part1());
+
+        log.info("Part2: " + stepCounter.part2());
         /*
         so the problem says how many garden plots can you reach in exactly 64 steps. Should I track state while processing by doing 1 or 2?
 
