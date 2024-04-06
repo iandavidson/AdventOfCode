1f76d58c1153606c1d092f3df94b23591c76016c
Ian Davidson <Ianmattdavidson@gmail.com>
2024-04-06 15:42:06 -0700

    init

---
+++ src/main/java/org/example/advent/year2022/day04/CampCleanup.java
@@ -0,0 +1,6 @@
+package org.example.advent.year2022.day04;
+
+public class CampCleanup {
+
+    
+}
---
+++ src/main/java/org/example/advent/year2022/day04/Pair.java
@@ -0,0 +1,3 @@
+package org.example.advent.year2022.day04;
+
+public record Pair(Range left, Range right){}
