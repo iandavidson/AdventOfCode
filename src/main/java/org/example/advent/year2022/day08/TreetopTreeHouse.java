b8109169f000fcdeac76b7db85a58fa9147fc6d1
Ian Davidson <Ianmattdavidson@gmail.com>
2024-04-08 17:31:49 -0700

    2022 day8 complete

--- src/main/java/org/example/advent/year2022/day08/TreetopTreeHouse.java
+++ src/main/java/org/example/advent/year2022/day08/TreetopTreeHouse.java
@@ -3,9 +3,11 @@ package org.example.advent.year2022.day08;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 public class TreetopTreeHouse {
 
@@ -15,13 +17,27 @@ public class TreetopTreeHouse {
     public static void main(String[] args) {
         TreetopTreeHouse treetopTreeHouse = new TreetopTreeHouse();
         System.out.println("Part1: " + treetopTreeHouse.part1());
+        System.out.println("Part2: " + treetopTreeHouse.part2());
     }
 
 
     public Long part1() {
         List<List<Integer>> grid = readInput();
-//        567 -> too low
         return countBoarderTrees(grid);
+    }
+
+    public Long part2() {
+        List<List<Integer>> grid = readInput();
+
+        long max = 0L;
+        for(int i = 1; i < grid.size()-1; i++){
+            for(int j = 1; j < grid.size()-1; j++){
+                long score = calculateTreeHouseScore(i,j,grid);
+//                System.out.println("row : " + i + "; col: " + j + " treeheight: "  + grid.get(i).get(j) + " ; score: " + score);
+                max = Math.max(max, score);
+            }
+        }
+        return max;
     }
 
     private List<List<Integer>> readInput() {
@@ -51,62 +67,101 @@ public class TreetopTreeHouse {
     }
 
     private long countBoarderTrees(List<List<Integer>> grid){
-        long count = 0L;
+        Set<String> canSee = new HashSet<>();
 
         //from left to right
-        for(List<Integer> row : grid){
-            long tempCount = 1;
-            for(int i =1; i < row.size(); i++){
-                if(row.get(i) > row.get(i-1)){
-                    tempCount++;
-                }else {
-                    break;
+        for(int i = 0; i < grid.size(); i++){
+            int max = -1;
+            for(int j =0; j < grid.get(i).size(); j++){
+                if(max < grid.get(i).get(j)){
+                    canSee.add(i + ":" + j);
+                    max = grid.get(i).get(j);
                 }
             }
-            count += tempCount;
         }
-
 
         //from top to bottom
         for(int i = 0; i < grid.get(0).size(); i++){
-            long tempCount = 1;
-            for(int j = 1; j <grid.size(); j++){
-                if(grid.get(j).get(i) > grid.get(j-1).get(i)){
-                    tempCount++;
-                }else{
-                    break;
+            int max = -1;
+            for(int j = 0; j < grid.size(); j++){
+                if(max < grid.get(j).get(i)){
+                    canSee.add(j + ":" + i);
+                    max = grid.get(j).get(i);
                 }
             }
-            count += tempCount;
         }
 
 
         //from right to left
         for(int i = 0; i < grid.size(); i++){
-            long tempCount = 1;
-            for(int j = grid.get(0).size()-2; j > -1; j--){
-                if(grid.get(i).get(j) <  grid.get(i).get(j-1)){
-                    tempCount++;
-                } else {
-                    break;
+            int max = -1;
+            for(int j = grid.get(0).size()-1; j > -1; j--){
+                if(max < grid.get(i).get(j)){
+                    max = grid.get(i).get(j);
+                    canSee.add(i + ":" + j);
                 }
             }
-            count+= tempCount;
+            //I still have the max
         }
 
         //from bottom to up
         for(int i = 0 ; i < grid.get(0).size(); i++){
-            long tempCount = 1;
-            for(int j = grid.size()-2; j > -1; j--){
-                if(grid.get(j).get(i) < grid.get(j-1).get(i)){
-                    tempCount++;
-                }else{
-                    break;
+            int max = -1;
+            for(int j = grid.size()-1; j > -1; j--){
+                if(max < grid.get(j).get(i)){
+                    max = grid.get(j).get(i);
+                    canSee.add(j + ":" + i);
                 }
             }
-            count += tempCount;
         }
 
-        return count;
+        return canSee.size();
+    }
+
+    private Long calculateTreeHouseScore(int row, int col, List<List<Integer>> grid){
+        int currentHeight = grid.get(row).get(col);
+
+        long left = 0L;
+        for(int i = col-1; i > -1; i--){
+            if(grid.get(row).get(i) < currentHeight){
+                left++;
+            }else {
+                left++;
+                break;
+            }
+        }
+
+        long right = 0L;
+        for(int i = col+1; i < grid.get(0).size(); i++){
+            if(grid.get(row).get(i) < currentHeight){
+                right++;
+            } else {
+                right++;
+                break;
+            }
+        }
+
+        long up = 0L;
+        for(int i = row -1; i > -1; i--){
+            if(grid.get(i).get(col) < currentHeight){
+                up++;
+            } else {
+                up++;
+                break;
+            }
+        }
+
+        long down = 0L;
+        for(int i = row+1; i < grid.size(); i++){
+            if(grid.get(i).get(col) < currentHeight){
+                down++;
+            } else {
+                down++;
+                break;
+            }
+        }
+
+
+        return left * right * up * down;
     }
 }
