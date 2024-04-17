b8bb75627de8a1db1057724ca6c30a2424bbe0d6
Ian Davidson <Ianmattdavidson@gmail.com>
2024-04-17 13:40:42 -0700

    now parsing input

--- src/main/java/org/example/advent/year2022/day13/DistressElement.java
+++ src/main/java/org/example/advent/year2022/day13/DistressElement.java
@@ -15,7 +15,7 @@ public class DistressElement {
 
     private static final Character L_B = '[';
     private static final Character R_B = ']';
-    private static final String COMMA = ",";
+    private static final Character COMMA = ',';
 
     @Builder.Default
     private List<DistressElement> innerPackets = new ArrayList<>();
@@ -28,21 +28,23 @@ public class DistressElement {
     public static DistressElement newDistressPacket(final String line) {
         if (line.charAt(0) == L_B) {
             String trimmedLine = line.substring(1, line.length() - 1);
-            List<DistressElement> innerPackets = new ArrayList<>();
-
-            //instead of this
-//            String[] elementArray = trimmedLine.split(COMMA);
-
-            /*
-            must only split on indexs that have bypassed an equal amount of '[' & ']'
-             */
-            for(int i = 0; i < trimmedLine.length(); i++){
-
+            if(trimmedLine.isEmpty()){
+                return DistressElement.builder().innerPackets(new ArrayList<>()).elementType(ElementType.EMPTY_LIST).build();
             }
 
+            List<DistressElement> innerPackets = new ArrayList<>();
 
-            for (String element : elementArray) {
-                innerPackets.add(newDistressPacket(element.trim()));
+            List<Integer> splitIndexes = findValidCommaIndexes(trimmedLine);
+            //add length of trimmed string so we have left and right boundaries for substring call
+            splitIndexes.add(trimmedLine.length());
+
+            int leftIndex = 0;
+            for(Integer rightIndex : splitIndexes){
+                innerPackets.add(
+                        newDistressPacket(trimmedLine.substring(leftIndex, rightIndex))
+                );
+
+                leftIndex = rightIndex + 1;
             }
 
             return DistressElement.builder().innerPackets(innerPackets).elementType(ElementType.LIST).build();
@@ -51,5 +53,22 @@ public class DistressElement {
             Integer value = Integer.parseInt(line);
             return DistressElement.builder().value(value).elementType(ElementType.SINGLE_ITEM).build();
         }
+    }
+
+    private static List<Integer> findValidCommaIndexes(String trimmedLine){
+        List<Integer> foundValidSplitIndexes = new ArrayList<>();
+        int left = 0;
+        int right = 0;
+        for(int i = 0; i < trimmedLine.length(); i++){
+            if(trimmedLine.charAt(i) == L_B){
+                left++;
+            } else if(trimmedLine.charAt(i) == R_B){
+                right++;
+            } else if(trimmedLine.charAt(i) == COMMA && left == right){
+                foundValidSplitIndexes.add(i);
+            }
+        }
+
+        return foundValidSplitIndexes;
     }
 }
--- src/main/java/org/example/advent/year2022/day13/ElementType.java
+++ src/main/java/org/example/advent/year2022/day13/ElementType.java
@@ -1,5 +1,5 @@
 package org.example.advent.year2022.day13;
 
 public enum ElementType {
-    SINGLE_ITEM, LIST;
+    SINGLE_ITEM, LIST, EMPTY_LIST;
 }
