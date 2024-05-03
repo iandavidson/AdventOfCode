2b23977ba0d9d05791251d66eb1e45ca0107f354
Ian Davidson <Ianmattdavidson@gmail.com>
2024-05-03 15:20:27 -0700

    further progress reading in inputs

--- src/main/java/org/example/advent/year2022/day19/Blueprint.java
+++ src/main/java/org/example/advent/year2022/day19/Blueprint.java
@@ -1,19 +1,38 @@
 package org.example.advent.year2022.day19;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public record Blueprint(int id, Cost oreRobot, Cost clayRobot, Cost obsidianRobot, Cost geodeRobot) {
+    @Getter
+    enum RESOURCE {
+        //Blueprint 1: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 15 clay. Each geode robot costs 2 ore and 20 obsidian.
+        ORE("ore"), CLAY("clay"), OBSIDIAN("obsidian");
 
-    public static Blueprint newBlueprint(final String line){
+        private final String wordName;
+
+        RESOURCE(String wordName) {
+            this.wordName = wordName;
+        }
+    }
+
+    public static Blueprint newBlueprint(final String line) {
         int id = Integer.parseInt(line.substring(10, line.indexOf(":")));
 
-        String robots =  line.split(":")[1].trim();
+        String robots = line.split(":")[1].trim();
 
-        String [] robotChunks = robots.split("\\.", 4);
+        String[] robotChunks = robots.split("\\.", 4);
         List<Cost> costs = new ArrayList<>();
-        for(String robotChunk : robotChunks){
-            String temp = robotChunk.substring(robotChunk.indexOf("costs") + 5).trim();
+        for (String robotChunk : robotChunks) {
+            String[] parts = robotChunk.substring(robotChunk.indexOf("costs") + 5).trim().split("\\s+");
+            if (parts.length == 2) {
+
+            } else {
+
+            }
+
             //"4 ore" OR "3 ore and 14 clay"
         }
 
