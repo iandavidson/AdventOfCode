cec26c8d8ca1e46f546b129be48e3fff1f930660
Ian Davidson <Ianmattdavidson@gmail.com>
2024-05-07 17:47:18 -0700

    comments added

--- src/main/java/org/example/kLowestOccurrences/KLowestOccurrences.java
+++ src/main/java/org/example/kLowestOccurrences/KLowestOccurrences.java
@@ -26,6 +26,7 @@ public class KLowestOccurrences {
 
     public int[] topKFrequent(int[] nums, int k) {
 
+        //build occurrence map {value} -> {# of value}
         Map<Integer, Integer> map = new HashMap<>();
         for (int i : nums) {
             if (map.containsKey(i)) {
@@ -35,6 +36,7 @@ public class KLowestOccurrences {
             }
         }
 
+        //build 2d array: 1st dimension = occurrence of value, 2nd dimension = list of values with that # of occurrences
         List<List<Integer>> count2d = new ArrayList<>(nums.length + 1);
         for (int i = 0; i < nums.length + 1; i++) {
             count2d.add(new ArrayList<>());
@@ -44,15 +46,17 @@ public class KLowestOccurrences {
             count2d.get(map.get(key)).add(key);
         }
 
+        //reverse list so we are able to traverse start to finish collecting the highest occurring values
         Collections.reverse(count2d);
 
+        //collect results with this list
         List<Integer> resultList = new ArrayList<>();
 
         for (List<Integer> inner : count2d) {
             if (inner.isEmpty()) {
                 continue;
             }
-
+            //e.g. if there are 2 spaces left but more than 2 element of the same occurrence, forgo adding any of them
             if(inner.size() <= k - resultList.size()){
                 for (Integer i : inner) {
                     resultList.add(i);
