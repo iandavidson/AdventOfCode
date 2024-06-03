e896f18a23aee066283bc85a3040fd85606ae99b
Ian Davidson <Ianmattdavidson@gmail.com>
2024-06-03 11:05:26 -0700

    day 14 complete

--- src/main/java/dev/davidson/ian/advent/year2015/day14/ReindeerOlympics.java
+++ src/main/java/dev/davidson/ian/advent/year2015/day14/ReindeerOlympics.java
@@ -17,47 +17,70 @@ public class ReindeerOlympics {
     private static final String INPUT_PATH = "adventOfCode/2015/day14/input.txt";
     private static final String SAMPLE_PATH = "adventOfCode/2015/day14/sample.txt";
     private static final Integer TOTAL_TIME = 2503;//1000;
+
     public static void main(String[] args) {
         ReindeerOlympics reindeerOlympics = new ReindeerOlympics();
         log.info("Part1: {}", reindeerOlympics.part1());
         log.info("Part2: {}", reindeerOlympics.part2());
     }
 
-    public int part1(){
+    public int part1() {
         List<Reindeer> reindeerList = readFile();
         int max = 0;
 
-        for(Reindeer reindeer: reindeerList){
-            int distanceTraveled = 0;
-            boolean currentlyResting = false;
-            int i = 0;
-            while(i < TOTAL_TIME){
-                if(currentlyResting){
-                    i += reindeer.restTime();
-                    currentlyResting = false;
-                }else{
-                    for(int j  = 0 ; i < TOTAL_TIME && j < reindeer.runTime(); j++){
-                        distanceTraveled += reindeer.runDistance();
-                        i++;
-                    }
-                    currentlyResting = true;
-                }
-            }
-            log.info("{}, {}", reindeer.name(), distanceTraveled);
-            max = Math.max(max, distanceTraveled);
+        for (Reindeer reindeer : reindeerList) {
+            max = Math.max(max, calculateDistanceAtSeconds(reindeer, TOTAL_TIME));
         }
         return max;
     }
 
-    public int part2(){
+    public int part2() {
         List<Reindeer> reindeerList = readFile();
-        List<ReindeerRaceState> reindeerRaceStates = reindeerList.stream().map(ReindeerRaceState::toReindeerRaceState).toList();
 
+        int maxScore = 0;
 
+        Map<Reindeer, Integer> map = new HashMap<>();
         //do it different this time
+        for (int i = 1; i < TOTAL_TIME; i++) {
+            int[] distances = new int[reindeerList.size()];
+            int max = 0;
+            List<Integer> leadIndex = new ArrayList<>();
+            for (int j = 0; j < reindeerList.size(); j++) {
+                distances[j] = calculateDistanceAtSeconds(reindeerList.get(j), i);
+                if (distances[j] > max) {
+                    leadIndex.clear();
+                    leadIndex.add(j);
+                    max = distances[j];
+                } else if (distances[j] == max) {
+                    leadIndex.add(j);
+                }
+            }
 
-        for(int i = 0; i < )
+            for (Integer lead : leadIndex) {
+                map.put(reindeerList.get(lead), map.getOrDefault(reindeerList.get(lead), 0) + 1);
+            }
+        }
 
+        for (Integer value : map.values()) {
+            maxScore = Math.max(maxScore, value);
+        }
+
+        return maxScore;
+
+    }
+
+    public int calculateDistanceAtSeconds(final Reindeer reindeer, final int seconds) {
+        int period = reindeer.runTime() + reindeer.restTime();
+        int cycles = seconds / period;
+        int totalDistance = cycles * reindeer.runDistance() * reindeer.runTime();
+        int remainder = seconds - cycles * period;
+        if (remainder > reindeer.runTime()) {
+            totalDistance += reindeer.runDistance() * reindeer.runTime();
+        } else {
+            totalDistance += reindeer.runDistance() * remainder;
+        }
+
+        return totalDistance;
     }
 
     private List<Reindeer> readFile() {
