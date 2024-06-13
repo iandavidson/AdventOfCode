53fe84a3f0edee4a6810c22d55f8e737017ec679
Ian Davidson <Ianmattdavidson@gmail.com>
2024-06-13 13:12:55 -0700

    both parts complete

--- src/main/java/dev/davidson/ian/advent/year2015/day21/Loadout.java
+++ src/main/java/dev/davidson/ian/advent/year2015/day21/Loadout.java
@@ -1,5 +1,30 @@
 package dev.davidson.ian.advent.year2015.day21;
 
-@Deprecated
-public record Loadout() {
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class Loadout {
+
+    private final List<Item> equipped = new ArrayList<>();
+
+    public void addToEquipped(Map<ItemType, List<Item>> shop, ItemType itemType, int index) {
+        if (index < 0) {
+            return;
+        }
+
+        equipped.add(shop.get(itemType).get(index));
+    }
+
+    public void removeFromEquipped(Map<ItemType, List<Item>> shop, ItemType itemType, int index) {
+        if (index < 0) {
+            return;
+        }
+
+        equipped.remove(shop.get(itemType).get(index));
+    }
+
 }
--- src/main/java/dev/davidson/ian/advent/year2015/day21/RPGSimulator.java
+++ src/main/java/dev/davidson/ian/advent/year2015/day21/RPGSimulator.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 @Slf4j
@@ -19,49 +20,51 @@ public class RPGSimulator {
 
     public static void main(String[] args) {
         RPGSimulator rpgSimulator = new RPGSimulator();
-        log.info("Part1: {}", rpgSimulator.part1());
+        rpgSimulator.execute();
     }
 
-    public int part1() {
+    public void execute() {
         Map<ItemType, List<Item>> shop = readShop();
         Player enemy = new Player(103, 9, 2);
 
-        int minCost = Integer.MAX_VALUE;
+        int minWinCost = Integer.MAX_VALUE; //part1
+        int maxLossCost = Integer.MIN_VALUE; //part2
 
-        //pick 1 weapon
+        Loadout loadout = new Loadout();
         for (int weapon = 0; weapon < shop.get(ItemType.Weapons).size(); weapon++) {
-            List<Item> equipped = new ArrayList<>();
-            addToEquipped(shop, ItemType.Weapons, weapon, equipped);
+            loadout.addToEquipped(shop, ItemType.Weapons, weapon);
 
             for (int armor = -1; armor < shop.get(ItemType.Armor).size(); armor++) {
-                addToEquipped(shop, ItemType.Armor, armor, equipped);
+                loadout.addToEquipped(shop, ItemType.Armor, armor);
 
                 for (int ring1 = -2; ring1 < shop.get(ItemType.Rings).size(); ring1++) {
-                    addToEquipped(shop, ItemType.Rings, ring1, equipped);
+                    loadout.addToEquipped(shop, ItemType.Rings, ring1);
 
                     for (int ring2 = ring1 + 1; ring2 < shop.get(ItemType.Rings).size(); ring2++) {
-                        addToEquipped(shop, ItemType.Rings, ring2, equipped);
+                        loadout.addToEquipped(shop, ItemType.Rings, ring2);
 
-                        int playerGearCost = equipped.stream().mapToInt(Item::value).sum();
-                        if (playerGearCost < minCost && battle(equipped, enemy)) {
-                            minCost = playerGearCost;
+                        int playerGearCost = loadout.getEquipped().stream().mapToInt(Item::value).sum();
+                        if (battle(loadout, enemy)) {
+                            minWinCost = Math.min(minWinCost, playerGearCost);
+                        } else {
+                            maxLossCost = Math.max(maxLossCost, playerGearCost);
                         }
 
-                        removeFromEquipped(shop, ItemType.Rings, ring2, equipped);
-
+                        loadout.removeFromEquipped(shop, ItemType.Rings, ring2);
                     }
-                    removeFromEquipped(shop, ItemType.Rings, ring1, equipped);
-
+                    loadout.removeFromEquipped(shop, ItemType.Rings, ring1);
                 }
-                removeFromEquipped(shop, ItemType.Armor, armor, equipped);
+                loadout.removeFromEquipped(shop, ItemType.Armor, armor);
             }
+            loadout.removeFromEquipped(shop, ItemType.Weapons, weapon);
         }
 
-        return minCost;
+        log.info("Part1: {}", minWinCost);
+        log.info("Part2: {}", maxLossCost);
     }
 
-    private boolean battle(List<Item> equipped, Player enemy) {
-        return battle(Player.newPlayer(PLAYER_HIT_POINTS, equipped), enemy);
+    private boolean battle(Loadout loadout, Player enemy) {
+        return battle(Player.newPlayer(PLAYER_HIT_POINTS, loadout.getEquipped()), enemy);
     }
 
     private boolean battle(Player player, Player enemy) {
@@ -74,7 +77,7 @@ public class RPGSimulator {
                 //player turn
                 enemyHealth = enemyHealth - Math.max(1, player.damage() - enemy.armor());
             } else {
-//                enemy turn
+                //enemy turn
                 playerHealth = playerHealth - Math.max(1, enemy.damage() - player.armor());
             }
             i++;
@@ -83,44 +86,8 @@ public class RPGSimulator {
         return playerHealth > 0;
     }
 
-    private void addToEquipped(Map<ItemType, List<Item>> shop, ItemType itemType, int index, List<Item> equipped) {
-        if (index < 0) {
-            return;
-        }
-
-        equipped.add(shop.get(itemType).get(index));
-    }
-
-    private void removeFromEquipped(Map<ItemType, List<Item>> shop, ItemType itemType, int index, List<Item> equipped) {
-        if (index < 0) {
-            return;
-        }
-
-        equipped.remove(shop.get(itemType).get(index));
-    }
-
     private Map<ItemType, List<Item>> readShop() {
-        List<List<String>> inputs = new ArrayList<>();
-
-        ClassLoader cl = RPGSimulator.class.getClassLoader();
-        File file = new File(cl.getResource(SHOP_PATH).getFile());
-        try {
-            Scanner scanner = new Scanner(file);
-            List<String> tempList = new ArrayList<>();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.isBlank()) {
-                    inputs.add(tempList);
-                    tempList = new ArrayList<>();
-                } else {
-                    tempList.add(line);
-                }
-            }
-            inputs.add(tempList);
-
-        } catch (FileNotFoundException e) {
-            throw new IllegalStateException(e.getCause());
-        }
+        List<List<String>> inputs = getInputLists();
 
         Map<ItemType, List<Item>> map = new HashMap<>();
 
@@ -140,34 +107,28 @@ public class RPGSimulator {
         return map;
     }
 
+    private static List<List<String>> getInputLists() {
+        List<List<String>> inputs = new ArrayList<>();
 
+        ClassLoader cl = RPGSimulator.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(cl.getResource(SHOP_PATH)).getFile());
+        try {
+            Scanner scanner = new Scanner(file);
+            List<String> tempList = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isBlank()) {
+                    inputs.add(tempList);
+                    tempList = new ArrayList<>();
+                } else {
+                    tempList.add(line);
+                }
+            }
+            inputs.add(tempList);
 
-    /*
-Weapons:    Cost  Damage  Armor
-Dagger        8     4       0
-Shortsword   10     5       0
-Warhammer    25     6       0
-Longsword    40     7       0
-Greataxe     74     8       0
-
-Armor:      Cost  Damage  Armor
-Leather      13     0       1
-Chainmail    31     0       2
-Splintmail   53     0       3
-Bandedmail   75     0       4
-Platemail   102     0       5
-
-Rings:      Cost  Damage  Armor
-Damage +1    25     1       0
-Damage +2    50     2       0
-Damage +3   100     3       0
-Defense +1   20     0       1
-Defense +2   40     0       2
-Defense +3   80     0       3
-
-Hit Points: 103
-Damage: 9
-Armor: 2
-
-     */
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException(e.getCause());
+        }
+        return inputs;
+    }
 }
