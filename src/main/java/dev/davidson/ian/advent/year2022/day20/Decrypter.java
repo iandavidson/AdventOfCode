fa7268935484a770463740b0d9ac663eee9ff776
Ian Davidson <Ianmattdavidson@gmail.com>
2024-06-28 17:54:52 -0700

    part2 complete

--- src/main/java/dev/davidson/ian/advent/year2022/day20/Decrypter.java
+++ src/main/java/dev/davidson/ian/advent/year2022/day20/Decrypter.java
@@ -1,32 +1,32 @@
 package dev.davidson.ian.advent.year2022.day20;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class Decrypter {
     private final List<Long> og;
     private final List<Element> altered;
 
     private final int cycles;
 
-    public Decrypter(final List<Long> input, final int cycles){
+    public Decrypter(final List<Long> input, final int cycles) {
         this.cycles = cycles;
         this.og = input;
         List<Element> elements = new ArrayList<>();
 
-        for(int i = 0; i < input.size(); i++){
+        for (int i = 0; i < input.size(); i++) {
             elements.add(new Element(i, input.get(i)));
         }
 
         this.altered = elements;
     }
 
-    private void reArrangeList()
-    {
-        for (int j = 0; j < cycles; j++)
-        {
-            for (int i = 0; i < og.size(); i++)
-            {
+    private void reArrangeList() {
+        for (int j = 0; j < cycles; j++) {
+            for (int i = 0; i < og.size(); i++) {
                 final Element elem = new Element(i, og.get(i));
                 long currentIndex = altered.indexOf(elem);
                 altered.remove(elem);
@@ -37,27 +37,27 @@ public class Decrypter {
         }
     }
 
-    private long modulo(final long index)
-    {
+    private long modulo(final long index) {
         final int denominator = og.size() - 1;
         long modulo = index % denominator;
-        if (modulo < 0)
-        {
+        if (modulo < 0) {
             modulo += denominator;
         }
         return modulo;
     }
 
-    public long decrypt()
-    {
+    public long decrypt() {
         reArrangeList();
 
-        final Element zeroElem = new Element(og.indexOf(0), 0);
-        final int zeroNodeNewIndex = altered.indexOf(zeroElem);
+        final Element zero = new Element(og.indexOf(0L), 0);
+        final int zeroIndex = altered.indexOf(zero);
 
-        return altered.get((zeroNodeNewIndex+1000) % og.size()).value()
-                + altered.get((zeroNodeNewIndex+2000) % og.size()).value()
-                + altered.get((zeroNodeNewIndex+3000) % og.size()).value();
+        long first = altered.get((zeroIndex + 1000) % og.size()).value();
+        long second = altered.get((zeroIndex + 2000) % og.size()).value();
+        long third = altered.get((zeroIndex + 3000) % og.size()).value();
+
+        log.info("{} + {} + {}", first, second, third);
+
+        return first + second + third;
     }
-
 }
--- src/main/java/dev/davidson/ian/advent/year2022/day20/GrovePositioningSystem.java
+++ src/main/java/dev/davidson/ian/advent/year2022/day20/GrovePositioningSystem.java
@@ -1,27 +1,35 @@
 package dev.davidson.ian.advent.year2022.day20;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+@Slf4j
 public class GrovePositioningSystem {
 
     private static final String SAMPLE_PATH = "adventOfCode/2022/day20/sample.txt";
     private static final String INPUT_PATH = "adventOfCode/2022/day20/input.txt";
+    private static final Long KEY = 811589153L;
 
     public static void main(String[] args) {
         GrovePositioningSystem grovePositioningSystem = new GrovePositioningSystem();
-        System.out.println("Part1: " + grovePositioningSystem.execute());
+        grovePositioningSystem.execute();
     }
 
-    public long execute(){
+    public void execute(){
         List<Long> original = readFile();
 
         Decrypter decrypter = new Decrypter(original, 1);
-        return decrypter.decrypt();
+        log.info("Part1: {}", decrypter.decrypt());
+
+        decrypter = new Decrypter(original.stream().map(val -> val * KEY).toList(), 10);
+        log.info("Part2: {}", decrypter.decrypt());
     }
 
     private List<Long> readFile(){
