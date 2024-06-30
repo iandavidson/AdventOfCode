ed1d071fc4694b64725afc53cf9ebba0c18a907a
Ian Davidson <Ianmattdavidson@gmail.com>
2024-06-29 17:25:30 -0700

    Part2 complete, time to clean up my code

--- src/main/java/dev/davidson/ian/advent/year2022/day21/MonkeyMath.java
+++ src/main/java/dev/davidson/ian/advent/year2022/day21/MonkeyMath.java
@@ -13,8 +13,11 @@ import java.util.Scanner;
 
 @Slf4j
 public class MonkeyMath {
+
     private static final String INPUT_PATH = "adventOfCode/2022/day21/input.txt";
+    private static final String SAMPLE_PATH = "adventOfCode/2022/day21/sample.txt";
     private static final String FINAL_MONKEY = "root";
+    private static final String HUMN = "humn";
 
     public static void main(String[] args) {
         MonkeyMath monkeyMath = new MonkeyMath();
@@ -54,10 +57,132 @@ public class MonkeyMath {
     }
 
 
-    public long part2(){
+    public long part2() {
         List<Monkey> monkeys = readFile();
         //humn is no longer valid
+        int n = monkeys.size();
+
+//        Monkey root = null;
+        Map<String, Long> monkeyValueMap = new HashMap<>();
+        Map<String, Monkey> monkeyNameMap = new HashMap<>();
+        for (Monkey monkey : monkeys) {
+            monkeyNameMap.put(monkey.getName(), monkey);
+
+            if (monkey.getDefaultValue() != null) {
+                if (monkey.getName().equals(HUMN)) {
+                    continue;
+                }
+                monkeyValueMap.put(monkey.getName(), monkey.getDefaultValue());
+            }
+        }
+
+//        long i =0L;
+        boolean stillWorking = true;
+        while (stillWorking) {
+            //if monkey has not been set
+            stillWorking = false;
+            for (int i = 0; i < n; i++) {
+                Monkey current = monkeys.get(i);
+                if (!monkeyValueMap.containsKey(current.getName())) {
+
+                    if (monkeyValueMap.containsKey(current.getDep1()) && monkeyValueMap.containsKey(current.getDep2())) {
+                        monkeyValueMap.put(
+                                current.getName(),
+                                OPERATION.executeOperation(current.getOperation(), monkeyValueMap.get(current.getDep1()), monkeyValueMap.get(current.getDep2()))
+                        );
+                        stillWorking = true;
+                    }
+                }
+
+            }
+        }
+
+        //should have tree filled out execpt for "humn" and any equation that depends on humn
+        // we know one of the paths from "root" is valid at this point.
+        Monkey root = monkeyNameMap.get(FINAL_MONKEY);
+        MonkeyValue monkeyValue = null;
+        if (monkeyValueMap.containsKey(root.getDep1())) {
+            monkeyValue = new MonkeyValue(root.getDep2(), monkeyValueMap.get(root.getDep1()));
+        } else {
+            monkeyValue = new MonkeyValue(root.getDep1(), monkeyValueMap.get(root.getDep2()));
+        }
+
+        while (true) {
+            monkeyValue = evaluateLabel(monkeyValue, monkeyValueMap, monkeyNameMap);
+
+            if (monkeyValue.name().equals(HUMN)) {
+                break;
+            }
+        }
+
+
+        return monkeyValueMap.get(HUMN);
     }
+
+    private MonkeyValue evaluateLabel(final MonkeyValue monkeyValue, Map<String, Long> monkeyValueMap, Map<String, Monkey> monkeyMap) {
+        // monkeyValue = b + c
+        Monkey current = monkeyMap.get(monkeyValue.name());
+
+        assert current != null;
+        MonkeyValue next = null;
+        if(monkeyValueMap.containsKey(current.getDep1())){
+            //b exists
+            long tempB = monkeyValueMap.get(current.getDep1());
+            String nextLabel = current.getDep2();
+
+            switch(current.getOperation()){
+                case ADD -> {
+                    //monkeyvalue = b + c;
+                    //result: c = monkeyValue - b
+                    next = new MonkeyValue(nextLabel, monkeyValue.value() - tempB);
+                }
+                case SUB -> {
+                    //monkeyvalue = b - c
+                    //result : c = b - monkeyValue
+                    next = new MonkeyValue(nextLabel, tempB - monkeyValue.value());
+                }
+                case MULT -> {
+                    //monkeyvalue = b * c
+                    //result : c = monkeyValue / b
+                    next = new MonkeyValue(nextLabel, monkeyValue.value() / tempB);
+                }
+                case DIV -> {//monkeyvalue = b / c
+                    //result : c = b / monkeyValue}
+                    next = new MonkeyValue(nextLabel, tempB / monkeyValue.value());
+                }
+            }
+        }else{
+            //c exists
+            long tempC = monkeyValueMap.get(current.getDep2());
+            String nextLabel = current.getDep1();
+            switch(current.getOperation()){
+                case ADD -> {
+                    //monkeyvalue = b + c
+                    //result : b = monkeyValue - c
+                    next = new MonkeyValue(nextLabel, monkeyValue.value()- tempC);
+                }
+                case SUB -> {
+                    //monkeyvalue = b - c
+                    //result : b = monkeyValue + c
+                    next = new MonkeyValue(nextLabel, monkeyValue.value() + tempC);
+                }
+                case MULT -> {
+                    //monkeyvalue = b * c
+                    //result : b = monkeyValue / c
+                    next = new MonkeyValue(nextLabel, monkeyValue.value() / tempC);
+                }
+                case DIV -> {
+                    //monkeyvalue = b / c
+                    //result : b = monkeyValue * c
+                    next = new MonkeyValue(nextLabel, monkeyValue.value() * tempC);
+                }
+            }
+        }
+
+        monkeyValueMap.put(next.name(), next.value());
+        return next;
+    }
+
     private List<Monkey> readFile() {
         List<Monkey> monkeys = new ArrayList<>();
 
---
+++ src/main/java/dev/davidson/ian/advent/year2022/day21/MonkeyValue.java
@@ -0,0 +1,5 @@
+package dev.davidson.ian.advent.year2022.day21;
+
+public record MonkeyValue(String name, Long value) {
+
+}
