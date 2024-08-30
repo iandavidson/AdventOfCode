11c5b1e034c877259c926802559e436b7d8c6242
Ian Davidson <Ianmattdavidson@gmail.com>
2024-08-30 09:09:06 -0700

    formatting

--- src/main/java/dev/davidson/ian/advent/year2016/day16/DragonChecksum.java
+++ src/main/java/dev/davidson/ian/advent/year2016/day16/DragonChecksum.java
@@ -16,35 +16,27 @@ public class DragonChecksum {
     private static final Integer SAMPLE_DISK_SPACE = 20;
 
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         DragonChecksum dragonChecksum = new DragonChecksum();
         log.info("Part1: {}", dragonChecksum.execute(INPUT_DISK_SPACE_PART_1));
         log.info("Part2: {}", dragonChecksum.execute(INPUT_DISK_SPACE_PART_2));
     }
 
-    public String execute(final Integer diskSpace){
+    public String execute(final Integer diskSpace) {
         String result = applyRule(readFile(), diskSpace);
         return generateCheckSum(result, diskSpace);
     }
 
-    private String applyRule(final String initial, final Integer diskSpace){
-/*
-Call the data you have at this point "a".
-Make a copy of "a"; call this copy "b".
-Reverse the order of the characters in "b".
-In "b", replace all instances of 0 with 1 and all 1s with 0.
-The resulting data is "a", then a single 0, then "b".
-         */
-
+    private String applyRule(final String initial, final Integer diskSpace) {
         String result = initial;
-        while(result.length() < diskSpace){
+        while (result.length() < diskSpace) {
             String a = result;
             String aPrime = new StringBuilder(a).reverse().toString();
             StringBuilder b = new StringBuilder();
-            for(int i = 0; i < aPrime.length(); i++){
-                if(aPrime.charAt(i) == '1'){
+            for (int i = 0; i < aPrime.length(); i++) {
+                if (aPrime.charAt(i) == '1') {
                     b.append('0');
-                }else{
+                } else {
                     b.append('1');
                 }
             }
@@ -55,14 +47,14 @@ The resulting data is "a", then a single 0, then "b".
         return result;
     }
 
-    private String generateCheckSum(final String value, final Integer diskSpace){
+    private String generateCheckSum(final String value, final Integer diskSpace) {
         StringBuilder checkSum = new StringBuilder(value.substring(0, diskSpace));
-        while(checkSum.length() % 2 == 0){
+        while (checkSum.length() % 2 == 0) {
             StringBuilder next = new StringBuilder();
-            for(int i = 0; i < checkSum.length(); i+=2){
-                if(checkSum.charAt(i) == checkSum.charAt(i+1)){
+            for (int i = 0; i < checkSum.length(); i += 2) {
+                if (checkSum.charAt(i) == checkSum.charAt(i + 1)) {
                     next.append('1');
-                }else{
+                } else {
                     next.append('0');
                 }
             }
@@ -73,13 +65,13 @@ The resulting data is "a", then a single 0, then "b".
         return checkSum.toString();
     }
 
-    public String readFile(){
+    public String readFile() {
         ClassLoader cl = DragonChecksum.class.getClassLoader();
         File file = new File(Objects.requireNonNull(cl.getResource(INPUT_PATH)).getFile());
-        try{
+        try {
             Scanner scanner = new Scanner(file);
             return scanner.nextLine();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new IllegalStateException("Couldn't read file at provided path");
         }
     }
