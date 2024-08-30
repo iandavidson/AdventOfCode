90a5f1c8e71bd878afc8ea590532f0e6d33cdcb5
Ian Davidson <Ianmattdavidson@gmail.com>
2024-08-30 12:11:15 -0700

    2016 day 17 complete

---
+++ src/main/java/dev/davidson/ian/advent/year2016/day17/Coordinate.java
@@ -0,0 +1,4 @@
+package dev.davidson.ian.advent.year2016.day17;
+
+public record Coordinate(int row, int col) {
+}
---
+++ src/main/java/dev/davidson/ian/advent/year2016/day17/DIRECTION.java
@@ -0,0 +1,5 @@
+package dev.davidson.ian.advent.year2016.day17;
+
+public enum DIRECTION {
+    U, D, L, R;
+}
