e53b2cf846b51d77b721a3f8871d7ab55709a3b3
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-01 13:28:49 -0700

    2016 day 18 complete

--- src/main/java/dev/davidson/ian/advent/year2016/day18/LikeARogue.java
+++ src/main/java/dev/davidson/ian/advent/year2016/day18/LikeARogue.java
@@ -13,21 +13,23 @@ import lombok.extern.slf4j.Slf4j;
 public class LikeARogue {
 
     private static final String INPUT_PATH = "adventOfCode/2016/day18/input.txt";
-    private static final Integer ROWS = 40;
+
     public static void main(String[] args) {
         LikeARogue likeARogue = new LikeARogue();
-        log.info("Part1: {}", likeARogue.part1());
+        log.info("Part1: {}", likeARogue.execute(39));
+        log.info("Part2: {}", likeARogue.execute(399999));
     }
 
-    public Long part1() {
+    public Long execute(final Integer totalRows) {
         String input = readFile();
         List<FloorTile> firstRow = Arrays.stream(input.split("")).map(str -> FloorTile.FLOOR_MAP.get(str)).toList();
 
         List<List<FloorTile>> hall = new ArrayList<>();
         hall.add(firstRow);
 
-        for(int i = 1; i < ROWS; i++){
-            hall.add(computeNextRow(hall.get(i)));
+        for (int i = 0; i < totalRows; i++) {
+            List<FloorTile> next = computeNextRow(hall.get(i));
+            hall.add(next);
         }
 
         return hall.stream()
@@ -40,38 +42,44 @@ public class LikeARogue {
 
     private List<FloorTile> computeNextRow(final List<FloorTile> current) {
         List<FloorTile> nextRow = new ArrayList<>();
-        for(int i = 0 ; i < floorTiles.size(); i++){
-            /*
-            ABCDE
-            12345
+        for (int i = 0; i < current.size(); i++) {
+            int count = getCount(
+                    getPreviousTile(current, i - 1),
+                    getPreviousTile(current, i),
+                    getPreviousTile(current, i + 1));
 
-The type of tile 2 is based on the types of tiles A, B, and C; the type of tile 5 is based on tiles D, E, and an imaginary "safe" tile. Let's call these three tiles from the previous row the left, center, and right tiles, respectively.
-Then, a new tile is a trap only in one of the following situations:
-
-Its left and center tiles are traps, but its right tile is not.
-Its center and right tiles are traps, but its left tile is not.
-Only its left tile is a trap.
-Only its right tile is a trap.
-In any other situation, the new tile is safe.
-             */
-            FloorTile left = getPreviousTile(current, i-1);
-            FloorTile center = getPreviousTile(current, i);
-            FloorTile right = getPreviousTile(current, i+1);
-
-
-
-            nextRow.add();
+            if (count == 1) {
+                nextRow.add(FloorTile.TRAP);
+            } else {
+                nextRow.add(FloorTile.SAFE);
+            }
         }
+        return nextRow;
     }
 
-    private FloorTile getPreviousTile(final List<FloorTile> current, final int index){
-        if(index < 0 || index >= current.size()){
-            return FloorTile.SAFE;
+    private int getCount(FloorTile left, FloorTile center, FloorTile right) {
+        int count = 0;
+        if (left == FloorTile.TRAP && center == FloorTile.TRAP && right == FloorTile.SAFE) {
+            count++;
         }
 
-        return current.get(index);
+        if (left == FloorTile.SAFE && center == FloorTile.TRAP && right == FloorTile.TRAP) {
+            count++;
+        }
+
+        if (left == FloorTile.TRAP && center == FloorTile.SAFE && right == FloorTile.SAFE) {
+            count++;
+        }
+
+        if (left == FloorTile.SAFE && center == FloorTile.SAFE && right == FloorTile.TRAP) {
+            count++;
+        }
+        return count;
     }
 
+    private FloorTile getPreviousTile(final List<FloorTile> current, final int index) {
+        return index < 0 || index >= current.size() ? FloorTile.SAFE : current.get(index);
+    }
 
     private String readFile() {
         ClassLoader cl = LikeARogue.class.getClassLoader();
