2f0da2deb3e42854f9f8d7854c2a22db946703aa
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-06 15:16:30 -0700

    added some ideas/notes

--- src/main/java/dev/davidson/ian/advent/year2016/day24/AirDuctSpelunking.java
+++ src/main/java/dev/davidson/ian/advent/year2016/day24/AirDuctSpelunking.java
@@ -28,7 +28,63 @@ public class AirDuctSpelunking {
 
         int r = 1;
         int c = 1;
+
+        /*
+        start at 1,1;
+
+         */
+
+
+        return 0;
     }
+
+    /*
+    Intuition:
+     Conditions for exercising different options:
+     ########
+     #0#A..##
+     #..1####
+     ###B...#
+     ########
+
+     start at 0, arrive at 1, we know we already counted 0->1 tiles, so the options should be between A & B
+
+     What conditions will influence our decision to pick?
+        Finding all on A will be 3 moves, and making it back to 1 will be 6 moves
+        Finding all on B will be 4 moves, and making it back to 1 will be 8 moves
+
+        options:
+        a.
+            0 -> 1 = 3 moves
+            1 -> A(all) -> 1 = 6
+            1 -> B(all) = 4
+            total: 13 moves
+        b.
+            0 -> 1 = 3 moves
+            1 -> B(all) -> 1 = 8
+            1 -> A(all) = 3
+            total: 14 moves
+
+        Can we calculate:
+         - shortest path to find all from a node
+         - shortest path to find all and return back to the same start node.
+
+        then if we have 2 options (other than how we came in)
+         - Prefer shortest round trip option of the two
+         -> add all found nodes to visited<>
+         - Then choose other option for find all nodes within
+
+        if we have 3 options (other than how we came in)
+         - prefer shortest roundtrip option of the three
+         -> add all found nodes to visited<>
+         - then choose 2nd shortest round trip option
+         -> add all found noddes to visited<>
+         - then choose remaining option and find all nodes within
+
+         Though the caveat here is that to calculate the shortest path to discover all nodes
+         We will need to build up our representation by calling recursively on a smaller subgraph
+         (to find optimal path for that subgraph).
+     */
 
     private List<List<Character>> readFile() {
         List<List<Character>> grid = new ArrayList<>();
