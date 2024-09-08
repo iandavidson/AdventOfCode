d569db76155fab42fefd4891ffb99c7cf8497d81
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-08 10:56:13 -0700

    day 24 complete

--- src/main/java/dev/davidson/ian/advent/year2016/day24/AirDuctSpelunking.java
+++ src/main/java/dev/davidson/ian/advent/year2016/day24/AirDuctSpelunking.java
@@ -17,7 +17,6 @@ public class AirDuctSpelunking {
     private static final String SAMPLE_PATH = "adventOfCode/2016/day24/sample.txt";
     private static final String INPUT_PATH = "adventOfCode/2016/day24/input.txt";
     private static final Character WALL = '#';
-    private static final Character SPACE = '.';
 
     private static final List<int[]> SHIFTS = List.of(
             new int[]{1, 0},
@@ -28,11 +27,12 @@ public class AirDuctSpelunking {
 
     public static void main(String[] args) {
         AirDuctSpelunking airDuctSpelunking = new AirDuctSpelunking();
-        log.info("Part1: {}", airDuctSpelunking.part1());
+        log.info("Part1: {}", airDuctSpelunking.execute(false));
+        log.info("Part2: {}", airDuctSpelunking.execute(true));
     }
 
 
-    public Integer part1() {
+    public Integer execute(boolean part2) {
         int[][] points = new int[8][2]; //r,c
         List<List<Character>> grid = readFile();
         int rows = grid.size();
@@ -56,8 +56,26 @@ public class AirDuctSpelunking {
                 distances[j][i] = distance;
             }
         }
+        List<List<Integer>> sequences = new ArrayList<>();
+        List<Integer> initial = new ArrayList<>();
+        initial.add(0);
+        permutations(List.of(1, 2, 3, 4, 5, 6, 7), initial, sequences);
 
-        return 0;
+        Integer minDistance = Integer.MAX_VALUE;
+        for (List<Integer> sequence : sequences) {
+            int temp = 0;
+            for (int i = 1; i < sequence.size(); i++) {
+                temp += distances[sequence.get(i - 1)][sequence.get(i)];
+            }
+
+            if (part2) {
+                temp += distances[sequence.get(sequence.size() - 1)][0];
+            }
+
+            minDistance = Math.min(minDistance, temp);
+        }
+
+        return minDistance;
     }
 
     private Integer shortestPath(final int[] start, final int[] finish, final List<List<Character>> grid) {
@@ -68,76 +86,78 @@ public class AirDuctSpelunking {
         queue.add(start);
 
         int distanceTraveled = 0;
-        while (queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int n = queue.size();
             for (int i = 0; i < n; i++) {
                 int[] current = queue.remove();
 
+                if (current[0] == finish[0] && current[1] == finish[1]) {
+                    return distanceTraveled;
+                }
+
                 List<int[]> neighbors = findNeighbors(current, grid);
+
+                for (int[] neighbor : neighbors) {
+                    if (!visited.contains(toKey(neighbor))) {
+                        queue.add(neighbor);
+                        visited.add(toKey(neighbor));
+                    }
+                }
             }
+            distanceTraveled++;
         }
 
         return -1;
     }
 
-
     private String toKey(final int[] rc) {
         return rc[0] + ":" + rc[1];
     }
 
-    /*
-    Intuition:
-     Conditions for exercising different options:
-     ########
-     #0#A..##
-     #..1####
-     ###B...#
-     ########
+    private void permutations(final List<Integer> remaining,
+                              final List<Integer> currentSequence,
+                              final List<List<Integer>> result) {
+        if (remaining.isEmpty()) {
+            result.add(currentSequence);
+            return;
+        }
 
-     start at 0, arrive at 1, we know we already counted 0->1 tiles, so the options should be between A & B
+        for (int i = 0; i < remaining.size(); i++) {
+            List<Integer> nextSeq = new ArrayList<>(currentSequence);
+            List<Integer> nextRemaining = new ArrayList<>(remaining);
+            nextSeq.add(nextRemaining.get(i));
+            nextRemaining.remove(i);
+            permutations(nextRemaining, nextSeq, result);
+        }
+    }
 
-     What conditions will influence our decision to pick?
-        Finding all on A will be 3 moves, and making it back to 1 will be 6 moves
-        Finding all on B will be 4 moves, and making it back to 1 will be 8 moves
 
-        options:
-        a.
-            0 -> 1 = 3 moves
-            1 -> A(all) -> 1 = 6
-            1 -> B(all) = 4
-            total: 13 moves
-        b.
-            0 -> 1 = 3 moves
-            1 -> B(all) -> 1 = 8
-            1 -> A(all) = 3
-            total: 14 moves
+    private List<int[]> findNeighbors(final int[] current, final List<List<Character>> grid) {
+        List<int[]> neighbors = new ArrayList<>();
 
-        Can we calculate:
-         - shortest path to find all from a node
-         - shortest path to find all and return back to the same start node.
+        for (int[] shift : SHIFTS) {
+            int nextR = current[0] + shift[0];
+            int nextC = current[1] + shift[1];
 
-        then if we have 2 options (other than how we came in)
-         - Prefer shortest round trip option of the two
-         -> add all found nodes to visited<>
-         - Then choose other option for find all nodes within
+            if (nextC > 0 &&
+                    nextC < grid.getFirst().size()
+                    && nextR > 0
+                    && nextR < grid.size()
+                    && grid.get(nextR).get(nextC) != WALL) {
+                neighbors.add(
+                        new int[]{nextR, nextC}
+                );
+            }
+        }
 
-        if we have 3 options (other than how we came in)
-         - prefer shortest roundtrip option of the three
-         -> add all found nodes to visited<>
-         - then choose 2nd shortest round trip option
-         -> add all found noddes to visited<>
-         - then choose remaining option and find all nodes within
-
-         Though the caveat here is that to calculate the shortest path to discover all nodes
-         We will need to build up our representation by calling recursively on a smaller subgraph
-         (to find optimal path for that subgraph).
-     */
+        return neighbors;
+    }
 
     private List<List<Character>> readFile() {
         List<List<Character>> grid = new ArrayList<>();
 
         ClassLoader cl = AirDuctSpelunking.class.getClassLoader();
-        File file = new File(Objects.requireNonNull(cl.getResource(SAMPLE_PATH)).getFile());
+        File file = new File(Objects.requireNonNull(cl.getResource(INPUT_PATH)).getFile());
         try {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
