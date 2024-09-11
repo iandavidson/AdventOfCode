ca9863512995115197a43ad8d33fd40d47cda3dc
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-11 09:18:55 -0700

    2017 Day3 complete

--- src/main/java/dev/davidson/ian/advent/year2017/day03/Coordinate.java
+++ src/main/java/dev/davidson/ian/advent/year2017/day03/Coordinate.java
@@ -1,4 +1,8 @@
 package dev.davidson.ian.advent.year2017.day03;
 
 public record Coordinate(int r, int c) {
+
+    public static Coordinate combineCoordinate(final Coordinate a, final Coordinate b){
+        return new Coordinate(a.r + b.r, a.c + b.c);
+    }
 }
--- src/main/java/dev/davidson/ian/advent/year2017/day03/SpiralMemory.java
+++ src/main/java/dev/davidson/ian/advent/year2017/day03/SpiralMemory.java
@@ -2,10 +2,11 @@ package dev.davidson.ian.advent.year2017.day03;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,55 +14,91 @@ public class SpiralMemory {
 
     private static final String INPUT_PATH = "adventOfCode/2017/day03/input.txt";
     private static final String SAMPLE_PATH = "adventOfCode/2017/day03/sample.txt";
+    private static final List<Coordinate> ALL_NEIGHBORS = List.of(
+            new Coordinate(-1, -1),
+            new Coordinate(-1, 0),
+            new Coordinate(-1, 1),
+            new Coordinate(0, -1),
+            new Coordinate(0, 1),
+            new Coordinate(1, -1),
+            new Coordinate(1, 0),
+            new Coordinate(1, 1)
+    );
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         SpiralMemory spiralMemory = new SpiralMemory();
-        Integer input = readFile(SAMPLE_PATH);
-        log.info("Part1: {}", spiralMemory.part1(input));
+        Integer input = readFile(INPUT_PATH);
+        spiralMemory.execute(input);
     }
 
-    public Integer part1(final Integer input){
+    public void execute(final Integer input) {
         int endRow = 0;
         int endCol = 0;
 
-        int currentRow = 1;
-        int currentCol = 2;
+        Coordinate current = new Coordinate(1, 0);
 
-        Direction dir = Direction.U;
-        Set<Coordinate> occupied = new HashSet<>();
-        occupied.add(new Coordinate(1,1));
-        for(int i = 0; i < input; i++){
-            //compute where we move to forthis turn
-            currentRow += Direction.SHIFT.get(dir)[0];
-            currentCol += Direction.SHIFT.get(dir)[1];
-            occupied.add(new Coordinate(currentRow, currentCol));
+        Direction dir = Direction.R;
+        Map<Coordinate, Long> occupied = new HashMap<>();
 
-            //
+        boolean foundPart2 = false;
+        long part2Value = 0L;
+
+        for (int i = 1; i < input + 1; i++) {
+            current = new Coordinate(current.r() + Direction.SHIFT.get(dir)[0],
+                    current.c() + Direction.SHIFT.get(dir)[1]);
+
+            if (i == 1) {
+                occupied.put(current, 1L);
+            } else {
+                long value = findNeighborSum(occupied, current);
+                if (!foundPart2 && value > input) {
+                    part2Value = value;
+                    foundPart2 = true;
+                }
+
+                occupied.put(current, value);
+            }
+
+
             Direction look = Direction.LOOK_MAP.get(dir);
-            int tempRow = currentRow + Direction.SHIFT.get(look)[0];
-            int tempCol = currentCol + Direction.SHIFT.get(look)[1];
+            Coordinate lookCoordinate = new Coordinate(current.r() + Direction.SHIFT.get(look)[0],
+                    current.c() + Direction.SHIFT.get(look)[1]);
 
-            if (!occupied.contains(new Coordinate(tempRow, tempCol))) {
+            if (i != 1 && !occupied.containsKey(lookCoordinate)) {
                 dir = look;
             }
 
-            endRow = currentRow;
-            endCol = currentCol;
+            if (i == input) {
+                endRow = current.r();
+                endCol = current.c();
+            }
         }
 
-        return Math.abs(endRow - 1) + Math.abs(endCol - 1);
+        log.info("Part1: {}", Math.abs(endRow - 1) + Math.abs(endCol - 1));
+        log.info("Part2: {}", part2Value);
     }
 
-    private static Integer readFile(final String filePath){
+    private long findNeighborSum(final Map<Coordinate, Long> map, final Coordinate current) {
+        Long sum = 0L;
+        for (Coordinate shift : ALL_NEIGHBORS) {
+            Coordinate potentialNeighbor = Coordinate.combineCoordinate(current, shift);
+            if (map.containsKey(potentialNeighbor)) {
+                sum += map.get(potentialNeighbor);
+            }
+        }
+
+        return sum;
+    }
+
+    private static Integer readFile(final String filePath) {
         ClassLoader cl = SpiralMemory.class.getClassLoader();
         File file = new File(Objects.requireNonNull(cl.getResource(filePath)).getFile());
-        try{
+        try {
             Scanner scanner = new Scanner(file);
             return Integer.parseInt(scanner.nextLine());
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new IllegalStateException("Couldn't read file at provided path");
         }
     }
-
 
 }
