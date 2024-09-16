72b4db3c8d87f8416f35a9a119cb02d03392e012
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-16 12:27:26 -0700

    part2 complete

--- src/main/java/dev/davidson/ian/advent/year2017/day10/KnotHash.java
+++ src/main/java/dev/davidson/ian/advent/year2017/day10/KnotHash.java
@@ -17,10 +17,16 @@ public class KnotHash {
 
     private static final String INPUT_PATH = "adventOfCode/2017/day10/input.txt";
     private static final Integer SIZE = 256;
+    private static final List<Integer> END = List.of(17, 31, 73, 47, 23);
+
     public static void main(String [] args){
         KnotHash knotHash = new KnotHash();
-        List<Integer> input = readFile(INPUT_PATH);
-        log.info("Part1: {}", knotHash.part1(input));
+        String input = readFile(INPUT_PATH);
+        log.info("Part1: {}",
+                knotHash.part1(Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).boxed().toList())
+        );
+
+        log.info("Part2: {}", knotHash.part2(input));
     }
 
     public Integer part1(final List<Integer> input){
@@ -28,19 +34,44 @@ public class KnotHash {
         int skip = 0;
         int currentIndex = 0;
         for(int shift : input){
-            int nextIndex = (currentIndex + shift + skip) % SIZE;
-            tieKnot(rope, currentIndex, nextIndex);
-            currentIndex = nextIndex;
+            tieKnot(rope, currentIndex, currentIndex + shift);
+            currentIndex = (currentIndex + shift + skip) % SIZE;
             skip++;
         }
-
 
         return rope.get(0) * rope.get(1);
     }
 
-    //mutates rope btw
+    public String part2(final String input){
+        byte [] bytes = input.getBytes();
+        List<Integer> inputs = IntStream.range(0, bytes.length).map(i -> bytes[i]).boxed().collect(Collectors.toList());
+        inputs.addAll(END);
+        List<Integer> rope = IntStream.range(0, SIZE).boxed().collect(Collectors.toList());
+
+        int skip = 0;
+        int currentIndex = 0;
+        for(int i = 0; i< 64; i++){
+            for(int shift : inputs){
+                tieKnot(rope, currentIndex, currentIndex + shift);
+                currentIndex = (currentIndex + shift + skip) % SIZE;
+                skip++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 16; i++){
+            Integer temp = rope.get(16 * i);
+            for(int j = 1; j < 16; j++){
+                temp ^= rope.get(16 * i + j);
+            }
+
+            sb.append(Integer.toHexString(temp));
+        }
+
+        return sb.toString();
+    }
+
     private void tieKnot(List<Integer> rope, final int currentIndex, final int finalIndex){
-        //identify subsequence, reverse, place back in initial space
         List<Integer> indexSequence = new ArrayList<>();
         for(int i = currentIndex; i  <  finalIndex; i++){
             indexSequence.add(i % SIZE);
@@ -53,20 +84,17 @@ public class KnotHash {
             valueSequence.add(rope.get(idx));
         }
 
-
         for(int i = 0; i < valueSequence.size(); i++){
             rope.set((i + currentIndex) % SIZE, valueSequence.get(i));
         }
-
-
     }
 
-    private static List<Integer> readFile(final String filePath){
+    private static String readFile(final String filePath){
         ClassLoader cl = KnotHash.class.getClassLoader();
         File file = new File(Objects.requireNonNull(cl.getResource(filePath)).getFile());
         try{
             Scanner scanner = new Scanner(file);
-            return Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).boxed().toList();
+            return scanner.nextLine();
         }catch(FileNotFoundException e){
             throw new IllegalStateException("Couldn't read file at provided path");
         }
