dbc6674f9bf2cb20527f7c3aa91629491f3d9bc8
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-22 17:30:10 -0700

    part1 done, working on part2

---
+++ src/main/java/dev/davidson/ian/advent/year2017/day14/Coordinate.java
@@ -0,0 +1,5 @@
+package dev.davidson.ian.advent.year2017.day14;
+
+public record Coordinate(int r, int c) {
+
+}
--- src/main/java/dev/davidson/ian/advent/year2017/day14/DiskDefragmentation.java
+++ src/main/java/dev/davidson/ian/advent/year2017/day14/DiskDefragmentation.java
@@ -3,9 +3,14 @@ package dev.davidson.ian.advent.year2017.day14;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigInteger;
-import java.util.HexFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,12 +18,14 @@ public class DiskDefragmentation {
 
     private static final String INPUT_PATH = "adventOfCode/2017/day14/input.txt";
     private static final String SAMPLE_PATH = "adventOfCode/2017/day14/sample.txt";
-
+    private static final Character EMPTY = '0';
+    private static final Character FILLED = '1';
 
     public static void main(String[] args) {
         DiskDefragmentation diskDefragmentation = new DiskDefragmentation();
         String input = readFile(SAMPLE_PATH);
         log.info("Part1: {}", diskDefragmentation.part1(input));
+        log.info("Part2: {}", diskDefragmentation.part2(input));
 
     }
 
@@ -39,24 +46,73 @@ public class DiskDefragmentation {
             String updatedKey = key + "-" + i;
             KnotHash knotHash = new KnotHash(updatedKey);
             String rowKey = knotHash.apply();
-
-            log.info("row: {}", rowKey);
-            String hexString = HexFormat.of().formatHex(rowKey.getBytes());
-            //TODO: looks like the hex string I generate isn't 32 chars, this throws off answer for sample input
-            log.info("row as hex: {}", hexString);
-            String binary = new BigInteger(hexString, 16).toString(2);
-            Integer bitCount = new BigInteger(hexString, 16).bitCount();
-            log.info("Binary: {}", binary);
-            log.info("Bit count: {}", bitCount);
-            count+= bitCount;
-
+            count += new BigInteger(rowKey, 16).bitCount();
         }
 
         return count;
     }
 
-//    static String hexToBin(String s) {
-//        return new BigInteger(s, 16).toString(2);
-//    }
+    private Integer part2(final String key) {
+        List<List<Character>> grid = new ArrayList<>();
+        Set<Coordinate> filledLocations = new HashSet<>();
+        for (int i = 0; i < 128; i++) {
+            String updatedKey = key + "-" + i;
+            KnotHash knotHash = new KnotHash(updatedKey);
+            String rowKey = knotHash.apply();
+            String binaryString = new BigInteger(rowKey, 16).toString(2);
+            List<Character> tempList = new ArrayList<>();
+            for (int j = 0; j < 128; j++) {
+                if(j < binaryString.length()){
+                    char ch = binaryString.charAt(j);
+                    tempList.add(ch);
+                    if (ch == '1') {
+                        filledLocations.add(new Coordinate(i, j));
+                    }
+                }else{
+                    tempList.add(EMPTY);
+                }
+            }
+            grid.add(tempList);
+        }
+
+        int foundGroups = 0;
+        Set<Coordinate> visited = new HashSet<>();
+
+        for (Coordinate coordinate : filledLocations) {
+            if (!visited.contains(coordinate)) {
+                BFS(grid, visited, coordinate);
+                foundGroups++;
+            }
+        }
+
+        return foundGroups;
+    }
+
+    private void BFS(final List<List<Character>> grid, final Set<Coordinate> visited, final Coordinate start) {
+        Queue<Coordinate> queue = new LinkedList<>();
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                Coordinate current = queue.remove();
+
+                for (int[] shift : List.of(new int[]{0, 1}, new int[]{0, -1}, new int[]{1, 0}, new int[]{-1, 0})) {
+                    Coordinate potentialNeighbor =
+                            new Coordinate(current.r() + shift[0], current.c() + shift[1]);
+
+                    if (potentialNeighbor.c() >= 0 && potentialNeighbor.c() <= 127
+                            && potentialNeighbor.r() >= 0 && potentialNeighbor.r() <= 127
+                            && grid.get(potentialNeighbor.r()).get(potentialNeighbor.c()) == FILLED
+                            && !visited.contains(potentialNeighbor)
+                    ) {
+                        queue.add(potentialNeighbor);
+                        visited.add(potentialNeighbor);
+                    }
+                }
+            }
+        }
+    }
 
 }
