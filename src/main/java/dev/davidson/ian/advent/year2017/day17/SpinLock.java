8189c44957f8640d60525193f83eb6df139145bd
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-24 21:35:17 -0700

    2017 day 17 complete

---
+++ src/main/java/dev/davidson/ian/advent/year2017/day17/SpinLock.java
@@ -0,0 +1,65 @@
+package dev.davidson.ian.advent.year2017.day17;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SpinLock {
+    private static final String INPUT_PATH = "adventOfCode/2017/day17/input.txt";
+    private static final String SAMPLE_PATH = "adventOfCode/2017/day17/sample.txt";
+    private static final Integer FINISH = 2017;
+
+    public static void main(String [] args){
+        SpinLock spinLock = new SpinLock();
+        Integer skipDistance = readFile(INPUT_PATH);
+        log.info("Part1: {}", spinLock.part1(skipDistance));
+        log.info("Part2: {}", spinLock.part2(skipDistance));
+    }
+
+    private static Integer readFile(final String path){
+        ClassLoader cl = SpinLock.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(cl.getResource(path)).getFile());
+        try{
+            Scanner scanner = new Scanner(file);
+            return Integer.parseInt(scanner.nextLine());
+        }catch(FileNotFoundException e){
+            throw new IllegalStateException("Couldn't read file at provided path");
+        }
+    }
+
+    public Integer part1(final Integer skipDistance){
+        List<Integer> state = new ArrayList<>(60000);
+        state.add(0);
+        int currentIndex = 0;
+        for(int i = 1; i <= FINISH; i++){
+            currentIndex = (currentIndex + skipDistance) % state.size();
+            state.add(currentIndex + 1, i);
+            currentIndex++;
+        }
+
+        return state.get((state.indexOf(FINISH) + 1) % state.size());
+    }
+
+    public Integer part2(final Integer skipDistance){
+        int totalTime = 50_000_001;
+        int currentIndex = 0;
+        int size = 1;
+        int res = 0;
+        for(int i = 1; i < totalTime; i ++){
+            currentIndex = (currentIndex + skipDistance) % size;
+            if(currentIndex == 0){
+                res = i;
+            }
+
+            size++;
+            currentIndex++;
+        }
+
+        return res;
+    }
+}
