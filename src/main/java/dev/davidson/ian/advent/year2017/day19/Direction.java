cba3fcb57201b7817028af1d411805c37699559e
Ian Davidson <Ianmattdavidson@gmail.com>
2024-09-26 12:49:21 -0700

    formatting

--- src/main/java/dev/davidson/ian/advent/year2017/day19/Direction.java
+++ src/main/java/dev/davidson/ian/advent/year2017/day19/Direction.java
@@ -5,7 +5,6 @@ import java.util.Map;
 public enum Direction {
     L, D, R, U;
 
-
     public static final Map<Direction, int[]> SHIFTS = Map.of(
             R, new int[]{0, 1},
             L, new int[]{0, -1},
